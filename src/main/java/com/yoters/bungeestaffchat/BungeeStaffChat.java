42738b1661a656aab4ff369b50957f6d23451360
Tony Costanzo <tonycos92@gmail.com>
2021-06-26 11:13:53 -0500

    Add basic config

--- src/main/java/com/yoters/bungeestaffchat/BungeeStaffChat.java
+++ src/main/java/com/yoters/bungeestaffchat/BungeeStaffChat.java
@@ -3,14 +3,23 @@ package com.yoters.bungeestaffchat;
 import com.yoters.bungeestaffchat.command.AdminChat;
 import com.yoters.bungeestaffchat.command.StaffChat;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class BungeeStaffChat extends Plugin {
+
+    public static BungeeStaffChat SINGLETON;
+    public Configuration config;
+
     @Override
     public void onEnable() {
-        getLogger().info("Trying to start Metrics");
-
-        getLogger().info("Metrics started successfully!");
-
+        BungeeStaffChat.SINGLETON = this;
+        loadConfig();
         getProxy().getPluginManager().registerCommand(this, new StaffChat());
         getProxy().getPluginManager().registerCommand(this, new AdminChat());
     }
@@ -18,5 +27,24 @@ public class BungeeStaffChat extends Plugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void loadConfig() {
+        if (!getDataFolder().exists())
+            getDataFolder().mkdir();
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream("config.yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
